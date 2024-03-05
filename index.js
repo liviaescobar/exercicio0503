00f70e67ab15621d3510e17a301c80453e0579f8
liviaescobar <126469722+liviaescobar@users.noreply.github.com>
2024-03-05 11:24:54 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log ("INTER VAI SER CAMPEÃO")
+
+const num1 = 20;
+const num2 = 654;
+
+let soma = num1 + num2;
+console.log ("O valor da soma é " + soma);
+
+let subtracao = num1 - num2;
+console.log ("O valor da subtracão é " + subtracao);
+
+let multiplicacao = num1 * num2;
+console.log ("O valor da multiplicacão é " + multiplicacao);
+
+let divisao = num1 / num2;
+console.log ("O valor da divisão é " + divisao);
